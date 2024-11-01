04cdb6f74f205958c86f867f7e06d6f0d74e3c44
CuriousTony <aszdorovtsev@yandex.ru>
2024-11-01 12:29:38 +0300

    Finished messing with CSS-selectors. Realised the perfection of the XPATH approach. Added writing to csv-file logics.

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,21 +7,28 @@ from selenium.webdriver.support import expected_conditions as ec
 
 
 def get_prices():
-    page = 'https://www.divan.ru/sankt-peterburg/category/pramye-divany'
+    page = 'https://www.divan.ru/sankt-peterburg/category/torshery'
     driver = webdriver.Chrome()
     print('Начинаем упражнение')
     driver.get(page)
 
+    WebDriverWait(driver, 5).until(ec.presence_of_all_elements_located(
+        (By.CLASS_NAME, 'WdR1o')))
+    raw_data = driver.find_elements(By.CLASS_NAME, 'WdR1o')
     try:
-        WebDriverWait(driver, 10).until(ec.presence_of_all_elements_located((By.CLASS_NAME, 'WdR1o')))
-        raw_data = driver.find_elements(By.CLASS_NAME, 'WdR1o')
-        for data in raw_data:
-            price = data.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU.KIkOH').text
-            print(price)
-
-    except Exception as e:
-        print(f"Ошибка в селекторах - {e}")
-
+        with open('data_to_df.csv', 'w', newline='', encoding='utf-8-sig') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Цена'])
+        try:
+            for data in raw_data:
+                price = data.find_element(By.XPATH, './/span[@data-testid="price"]').text.replace('руб.', '').strip()
+                with open('data_to_df.csv', 'a', newline='', encoding='utf-8-sig') as file:
+                    writer = csv.writer(file)
+                    writer.writerow([price])
+        except Exception as e:
+            print(f"Ошибка в ходе записи в файл - {e}")
+    except Exception as ex:
+        print(f"Ошибка в ходе парсинга цен - {ex}")
     finally:
         print("Закончили упражнение (finally).")
         driver.quit()
